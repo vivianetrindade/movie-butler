b980a9ac010dae215fd631bd2f7a05453b284237
Salt Dev <saltdev@Salts-MacBook-Pro-3.local>
2022-07-08 08:34:39 +0200

    fixed some stuffs

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,16 +4,12 @@ import {useNavigate} from "react-router-dom";
 const Movie = ({ movie }) => {
     const navigate = useNavigate();
     return (
-        <section className="movie">
+        <section className="movie" onClick={() => navigate(`/info/${movie.id}`)}>
             
             <img className="movie__image" src={movie.image} alt={movie.title} />
             <div className="movie__info">
                 <h3 className="movie__title">{movie.title}</h3>
                 <span className="movie__rating">{movie.imDbRating}</span>
-            </div>
-            <div className="movie__buttons">
-
-                <button className="movie__button" onClick={() => navigate(`/info/${movie.id}`)}>More info...</button>
             </div>
         </section>
     );
--- client/src/components/Serie.js
+++ client/src/components/Serie.js
@@ -4,16 +4,12 @@ import {useNavigate} from "react-router-dom";
 const Serie = ({ serie }) => {
     const navigate = useNavigate();
     return (
-        <section className="movie">
+        <section className="movie" onClick={() => navigate(`/info/${serie.id}`)}>
             
             <img className="movie__image" src={serie.image} alt={serie.title} />
             <div className="movie__info">
-            <p className="movie__title">{serie.title}</p>
-            <p className="movie__rating">{serie.imDbRating}</p>
-            </div>
-            <div className="movie__buttons">
-
-                <button className="movie__button" onClick={() => navigate(`/info/${serie.id}`)}>More info...</button>
+                <p className="movie__title">{serie.title}</p>
+                <p className="movie__rating">{serie.imDbRating}</p>
             </div>
         </section>
     );
